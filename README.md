2c109e58adcd7f8f6635dbd1376ad8a3993673f4
Sinclair <truly.sinclair@gmail.com>
2025-05-25 18:34:30 -0400

    feat: add autogenerated OpenAPI TypeScript definitions
    Introduce OpenAPI TypeScript definitions for xsi-actions and xsi-events. These files were generated automatically, so manual edits should be avoided. Updated `tsconfig.json` to include declaration output, and added required dependencies to `yarn.lock`.
    
    Signed-off-by: Sinclair <truly.sinclair@gmail.com>

---
+++ README.md
@@ -0,0 +1,7 @@
+# The Unofficial Cisco Broadworks Xtended Services Interface DevKit
+
+## Installation & Setup
+
+## Usage
+
+### Requesting a Subscription
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,56 @@
-import {readdir} from 'node:fs/promises'
+import createClient from "openapi-fetch";
+import {paths as xsiActionsPaths} from "../types/xsi-actions.js"
+import {paths as xsiEventsPaths} from "../types/xsi-events.js"
 
-const files = await readdir('./')
-
-for (const file of files) {
-  console.log(file)
+type ClientConfig = {
+  /** The hostname for the endpoint. (i.e., https://<subdomains>.nextiva.com) */
+  hostname: string
+  /** The username for authentication. (XSI-specific) */
+  username: string
+  /** The password for authentication. (XSI-specific) */
+  password: string
+  /** Instruct XSI to send JSON responses. */
+  useJson?: boolean
+  /** Optional base path for the XSI-Actions API. Defaults to `"com.broadsoft.xsi-<actions|events>/v2.0"` if not provided. */
+  xsiBasePath?: string
 }
+
+/**
+ * Creates a client instance configured for BroadSoft XSI-Actions API interactions.
+ */
+export const xsiActions = ({
+  hostname,
+  username,
+  password,
+  xsiBasePath
+}: ClientConfig): ReturnType<typeof createClient<xsiActionsPaths>> => createClient<xsiActionsPaths>({
+  baseUrl: new URL(xsiBasePath || "com.broadsoft.xsi-actions/v2.0", hostname).toString(),
+  headers: {
+    "Authorization": `Basic ${Buffer.from(`${username}:${password}`).toString("base64")}`,
+    // TODO make json optional
+    'Accept': 'application/json',
+    'Content-Type': 'application/json',
+  }
+})
+
+/**
+ * Creates a client instance configured for BroadSoft XSI-Events API interactions.
+ */
+export const xsiEvents = ({
+  hostname,
+  username,
+  password,
+  xsiBasePath
+}: ClientConfig): ReturnType<typeof createClient<xsiEventsPaths>> => createClient<xsiEventsPaths>({
+  baseUrl: new URL(xsiBasePath || "com.broadsoft.xsi-events/v2.0", hostname).toString(),
+  headers: {
+    "Authorization": `Basic ${Buffer.from(`${username}:${password}`).toString("base64")}`,
+    // TODO make json optional
+    'Accept': 'application/json',
+    'Content-Type': 'application/json',
+  }
+})
+
+const t = xsiActions({
+  ho
+})
